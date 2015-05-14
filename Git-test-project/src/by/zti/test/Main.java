d99106af45f41038effd3cedae624a70fafa7be8
dmitriy.muratov <ongift@ya.ru>
2015-05-14 17:41:08 +0300

    добавил 2

--- Git-test-project/src/by/zti/test/Main.java
+++ Git-test-project/src/by/zti/test/Main.java
@@ -2,7 +2,7 @@ package by.zti.test;
 
 public class Main {
 	public static void main(String[] args){
-		System.out.println("Hello from Git Hub!1!!");
+		System.out.println("Hello from Git Hub!1!2!");
 	}
 
 }
